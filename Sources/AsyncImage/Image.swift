6249b888e8e1bac4900fdac92b39e42c6bb97906
kkla320 <accounts_kkla@icloud.com>
2021-07-12 16:50:07 +0200

    Added support for watchOS

--- Sources/AsyncImage/Image.swift
+++ Sources/AsyncImage/Image.swift
@@ -6,13 +6,13 @@
 //
 
 import Foundation
-#if os(iOS)
+#if os(iOS) || os(watchOS)
 import UIKit
 #elseif os(macOS)
 import AppKit
 #endif
 
-#if os(iOS)
+#if os(iOS) || os(watchOS)
 public typealias Image = UIImage
 #elseif os(macOS)
 public typealias Image = NSImage
